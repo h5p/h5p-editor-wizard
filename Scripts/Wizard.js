d3cb4f4858755cfb0499c011b45de6f87e28feba
Paal Joergensen <pal.jorgensen@gmail.com>
2018-10-04 15:07:01 +0200

    Remove unused variable

--- Scripts/Wizard.js
+++ Scripts/Wizard.js
@@ -80,7 +80,7 @@ H5PEditor.widgets.wizard = H5PEditor.Wizard = (function ($, EventDispatcher) {
     });
 
     // Create wizard navigation wrapper and attach buttons
-    var $navButtonsWrapper = $('<div class="h5peditor-wizard-navigation-buttons"></div>')
+    $('<div class="h5peditor-wizard-navigation-buttons"></div>')
       .append($prevButton)
       .append($nextButton)
       .appendTo(this.$item);
